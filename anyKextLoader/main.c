92e095815bc743a4f0e6a910f69248a9ac992176
Linus Henze <linus.henze@gmx.de>
2016-03-22 16:51:13 +0100

    Initial Commit

---
+++ anyKextLoader/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  anyKextLoader
+//
+//  Created by Linus Henze on 22/03/16.
+//  Copyright © 2016 Linus Henze. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
